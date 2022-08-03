d88790e8f253aba15f42f94da2c8f38931a0a730
Matias Bertani <matiasbertani@gmail.com>
2022-08-03 16:48:31 -0300

    test initial game

---
+++ wumpus/test_game.py
@@ -0,0 +1,12 @@
+import unittest
+from wumpus.game import WumpusGame
+from wumpus.constants import IN_PROGRESS
+
+
+class TestTable(unittest.TestCase):
+
+    def setUp(self):
+        self.game = WumpusGame()
+
+    def test_initial_game(self):
+        self.assertEqual(self.game.state, IN_PROGRESS)
