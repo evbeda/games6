87f8bd41c084a9cac5c275821709694f6e503b7a
natilou <natali.fpaura@gmail.com>
2022-08-03 16:22:02 -0300

    test select player random

---
+++ backgammon/tests/test_select.py
@@ -0,0 +1,20 @@
+import unittest
+from select_player import select_player
+from unittest.mock import patch
+
+
+class SelectPalyerTest(unittest.TestCase):
+
+    @patch('random.randint', return_value=1)
+    def test_player_1(self, patched_randint):
+        player = select_player()
+        self.assertEqual(player, 1)
+
+    @patch('random.randint', return_value=2)
+    def test_player_2(self, patched_randint):
+        player = select_player()
+        self.assertEqual(player, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
