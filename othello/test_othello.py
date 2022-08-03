96856175cf83619108311840d4e8306953ee0d28
lgiordano <lucogiordano@gmail.com>
2022-08-03 16:17:48 -0300

    Othello init board test

--- othello/test_othello.py
+++ othello/test_othello.py
@@ -1,5 +1,18 @@
 import unittest
+from othello.othello_game import Othello
 
 
 class Test_othello(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.game = Othello()
+        self.board = self.game.init_board
+
+    def test_board_size(self):
+        self.assertEqual(len(self.board), 8)
+        for row in self.board:
+            self.assertEqual(len(row), 8)
+
+
+if __name__ == "__main__":
+    unittest.main()
