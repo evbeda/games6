d030476064c734a3450a9e3f5ff58137d5ed919f
lgiordano <lucogiordano@gmail.com>
2022-08-03 18:01:44 -0300

    test_east started

--- othello/test_othello.py
+++ othello/test_othello.py
@@ -1,5 +1,6 @@
 import unittest
 from othello.othello_game import Othello
+from parameterized import parameterized
 
 
 class Test_othello(unittest.TestCase):
@@ -12,6 +13,23 @@ class Test_othello(unittest.TestCase):
         self.assertEqual(len(self.board), 8)
         for row in self.board:
             self.assertEqual(len(row), 8)
+
+    # 0 is a black win, 1 is a white win, None there are still moves available
+    @parameterized.expand([
+        ([
+            ["B", "W", "W", "W", "W", "B", "W", "W"],
+            ["B", "B", "B", "W", "W", "W", "W", "W"],
+            ["B", "W", "B", "W", "W", "W", "B", "W"],
+            ["B", "B", "B", "W", "W", "W", "W", "B"],
+            ["B", "B", "B", "W", "B", "B", "W", "B"],
+            ["B", "B", "W", "B", "W", "B", "B", "B"],
+            ["B", "B", "B", "W", "W", "B", "B", "B"],
+            ["B", "W", "B", "B", "B", "B", "B", "B"]], 37, 27)
+    ])
+    def test_pieces_count(self, final_board, expected_black, expected_white):
+        result = Othello.pieces_count(final_board)
+        self.assertEqual(result[0], expected_black)
+        self.assertEqual(result[1], expected_white)
 
 
 if __name__ == "__main__":
