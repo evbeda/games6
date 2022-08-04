889ff9041d79cfe0f0fc56f7f56b03cd2cffbffb
lgiordano <lucogiordano@gmail.com>
2022-08-04 13:22:16 -0300

    test available pieces

--- backgammon/tests/test_board.py
+++ backgammon/tests/test_board.py
@@ -1,12 +1,34 @@
 import unittest
-from ..game.create_board import create_board
+from parameterized import parameterized
+from ..game.create_board import create_board, available_pieces
 
 
 class BoardTest(unittest.TestCase):
 
+    def setUp(self):
+        self.expected_board = [
+            [0, 0],
+            [2, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 5],
+            [0, 0], [0, 3], [0, 0], [0, 0], [0, 0], [5, 0],
+            [0, 5], [0, 0], [0, 0], [0, 0], [0, 0], [3, 0],
+            [0, 0], [5, 0], [0, 0], [0, 0], [0, 0], [0, 2]]
+
+        self.create_board = create_board()
+
     def test_slots(self):
-        board = create_board()
-        self.assertEqual(len(board), 24)
+        self.assertEqual(len(self.create_board), 25)
+
+    def test_initial_board(self):
+        self.assertListEqual(self.expected_board, self.create_board)
+
+    @parameterized.expand(
+        [
+            ("W", [1, 12, 18, 20]),
+            ("B", [6, 8, 13, 24])]
+    )
+    def test_availabel_pieces(self, side, expected):
+        result = available_pieces(self.expected_board, side)
+        self.assertListEqual(result, expected)
 
 
 if __name__ == '__main__':
