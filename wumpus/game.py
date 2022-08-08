ab876b9683cae75ae8dd37cf61315d27e5357103
natilou <natali.fpaura@gmail.com>
2022-08-08 12:34:43 -0300

    move player to game class

--- wumpus/game.py
+++ wumpus/game.py
@@ -9,3 +9,11 @@ class WumpusGame:
 
     def place_player(self):
         self.board[0][0] = "J"
+
+    def position_finder(self, item):
+        position_list = []
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == item:
+                    position_list.append((i, j))
+        return position_list
--- wumpus/test_game.py
+++ wumpus/test_game.py
@@ -1,6 +1,8 @@
 import unittest
 from wumpus.game import WumpusGame
 from wumpus.constants import IN_PROGRESS, COL, ROW
+from parameterized import parameterized
+from .scenarios import SCENARIO_1, SCENARIO_2, SCENARIO_3, SCENARIO_4
 
 
 class TestGame(unittest.TestCase):
@@ -21,3 +23,14 @@ class TestGame(unittest.TestCase):
     def test_initial_place_player(self):
         self.game.place_player()
         self.assertEqual(self.game.board[0][0], "J")
+
+    @parameterized.expand([
+        (SCENARIO_1, "J", [(0, 0)]),
+        (SCENARIO_2, "J", [(1, 3)]),
+        (SCENARIO_3, "J", [(3, 5)]),
+        (SCENARIO_4, "J", [])
+    ])
+    def test_position_finder(self, board, item, expected):
+        self.game.board = board
+        position_list = self.game.position_finder(item)
+        self.assertEqual(position_list, expected)
