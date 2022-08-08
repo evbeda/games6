10bd8dd91c68ba80b3f5b8b7844c4a95d252f35b
lgiordano <lucogiordano@gmail.com>
2022-08-08 11:54:40 -0300

    move table to game class

--- wumpus/game.py
+++ wumpus/game.py
@@ -1,7 +1,11 @@
-from wumpus.constants import IN_PROGRESS
+from wumpus.constants import IN_PROGRESS, COL, ROW
 
 
 class WumpusGame:
 
     def __init__(self) -> None:
         self.state = IN_PROGRESS
+        self.board = [[None for j in range(COL)] for i in range(ROW)]
+
+    def place_player(self):
+        self.board[0][0] = "J"
--- wumpus/test_game.py
+++ wumpus/test_game.py
@@ -1,6 +1,6 @@
 import unittest
 from wumpus.game import WumpusGame
-from wumpus.constants import IN_PROGRESS
+from wumpus.constants import IN_PROGRESS, COL, ROW
 
 
 class TestGame(unittest.TestCase):
@@ -10,3 +10,14 @@ class TestGame(unittest.TestCase):
 
     def test_initial_game(self):
         self.assertEqual(self.game.state, IN_PROGRESS)
+
+    def test_board_columns(self):
+        for row in self.game.board:
+            self.assertEqual(len(row), COL)
+
+    def test_board_rows(self):
+        self.assertEqual(len(self.game.board), ROW)
+
+    def test_initial_place_player(self):
+        self.game.place_player()
+        self.assertEqual(self.game.board[0][0], "J")
