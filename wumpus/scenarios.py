e836b79492f79981ea565705279cdafa5e843c65
natilou <natali.fpaura@gmail.com>
2022-08-08 12:34:43 -0300

    move player to game class

---
+++ wumpus/scenarios.py
@@ -0,0 +1,12 @@
+from .constants import ROW, COL
+
+SCENARIO_1 = [[None for j in range(COL)] for i in range(ROW)]
+SCENARIO_1[0][0] = "J"
+
+SCENARIO_2 = [[None for j in range(COL)] for i in range(ROW)]
+SCENARIO_2[1][3] = "J"
+
+SCENARIO_3 = [[None for j in range(COL)] for i in range(ROW)]
+SCENARIO_3[3][5] = "J"
+
+SCENARIO_4 = [[None for j in range(COL)] for i in range(ROW)]
