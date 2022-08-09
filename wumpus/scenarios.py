bba677aec88c9b9714d7bf12c2472f18b4f49556
Matias Bertani <matiasbertani@gmail.com>
2022-08-09 12:31:13 -0300

    there is gold

--- wumpus/scenarios.py
+++ wumpus/scenarios.py
@@ -1,4 +1,4 @@
-from .constants import ROW, COL
+from .constants import GOLD, ROW, COL
 
 SCENARIO_1 = [[None for j in range(COL)] for i in range(ROW)]
 SCENARIO_1[0][0] = "J"
@@ -13,3 +13,12 @@ SCENARIO_4 = [[None for j in range(COL)] for i in range(ROW)]
 
 SCENARIO_5 = [[None for j in range(COL)] for i in range(ROW)]
 SCENARIO_5[2][4] = "J"
+
+
+SCENARIO_TEST_GOLD = [[None for j in range(COL)] for i in range(ROW)]
+SCENARIO_TEST_GOLD[2][4] = GOLD
+SCENARIO_TEST_GOLD[4][5] = GOLD
+SCENARIO_TEST_GOLD[7][14] = GOLD
+SCENARIO_TEST_GOLD[3][4] = GOLD
+SCENARIO_TEST_GOLD[6][4] = GOLD
+SCENARIO_TEST_GOLD[5][5] = GOLD
