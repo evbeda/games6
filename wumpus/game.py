1be0415f53e2f71beda9d0c34f4a78c6884b9654
natilou <natali.fpaura@gmail.com>
2022-08-09 11:15:47 -0300

    refactoring names and attributes

--- wumpus/game.py
+++ wumpus/game.py
@@ -1,4 +1,9 @@
-from wumpus.constants import IN_PROGRESS, COL, ROW
+from wumpus.constants import (
+    IN_PROGRESS,
+    COL,
+    ROW,
+    SWORDS_QUANTITY
+)
 
 
 class WumpusGame:
@@ -6,6 +11,9 @@ class WumpusGame:
     def __init__(self) -> None:
         self.state = IN_PROGRESS
         self.board = [[None for j in range(COL)] for i in range(ROW)]
+        self.place_player()
+        self.swords = SWORDS_QUANTITY
+        self.collected_gold = 0
 
     def place_player(self):
         self.board[0][0] = "J"
@@ -18,23 +26,23 @@ class WumpusGame:
                     position_list.append((i, j))
         return position_list
 
-    def is_not_out_of_bounds(self, coordinate, direction):
+    def is_not_out_of_bounds(self, row, col, direction):
         validator = True
-        if direction == "w" and not coordinate[0] > 0:
+        if direction == "w" and not row > 0:
             validator = False
-        if direction == "s" and not coordinate[0] < 14:
+        if direction == "s" and not row < 14:
             validator = False
-        if direction == "a" and not coordinate[1] > 0:
+        if direction == "a" and not col > 0:
             validator = False
-        if direction == "d" and not coordinate[1] < 7:
+        if direction == "d" and not col < 7:
             validator = False
         return validator
 
-    def check_is_empty(self, coordinate):
-        cell = self.board[coordinate[0]][coordinate[1]]
-        return True if cell is None else False
+    def check_is_empty(self, row, col):
+        cell = self.board[row][col]
+        return cell is None
 
-    def move_J_transaction(self, coordinate):
+    def move_player_transaction(self, row, col):
         pos = self.position_finder("J")[0]
-        self.board[coordinate[0]][coordinate[1]] = "J"
+        self.board[row][col] = "J"
         self.board[pos[0]][pos[1]] = None
