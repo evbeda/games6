b743a23aa3f7925b8cce2385c667b64492e196ae
Mario Merida <mamerida2018@gmail.com>
2022-08-09 16:00:16 -0300

    find signal position

--- wumpus/scenarios.py
+++ wumpus/scenarios.py
@@ -14,7 +14,6 @@ SCENARIO_4 = [['' for j in range(COL)] for i in range(ROW)]
 SCENARIO_5 = [['' for j in range(COL)] for i in range(ROW)]
 SCENARIO_5[2][4] = "J"
 
-
 SCENARIO_TEST_GOLD = [['' for j in range(COL)] for i in range(ROW)]
 SCENARIO_TEST_GOLD[2][4] = GOLD
 SCENARIO_TEST_GOLD[4][5] = GOLD
@@ -30,4 +29,36 @@ SCENARIO_TEST_DELETE[7][8] = WUMPUS
 SCENARIO_TEST_DELETE[7][14] = WUMPUS
 SCENARIO_TEST_DELETE[2][10] = GOLD
 SCENARIO_TEST_DELETE[3][4] = PLAYER
+
 # include in this scenario the signal of danger when they are already encoded
+SCENARIO_DANGER_SIGNAL_HOLES = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_SIGNAL_HOLES[0][0] = "J"
+SCENARIO_DANGER_SIGNAL_HOLES[1][1] = "O"
+
+SCENARIO_DANGER_LEFT_DOWN = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_LEFT_DOWN[0][0] = "J"
+SCENARIO_DANGER_LEFT_DOWN[7][0] = "O"
+
+SCENARIO_DANGER_RIGTH_DOWN = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_RIGTH_DOWN[0][0] = "J"
+SCENARIO_DANGER_RIGTH_DOWN[7][7] = "O"
+
+SCENARIO_DANGER_RIGTH_UP = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_RIGTH_UP[0][0] = "J"
+SCENARIO_DANGER_RIGTH_UP[0][7] = "O"
+
+SCENARIO_DANGER_LEFT = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_LEFT[0][0] = "J"
+SCENARIO_DANGER_LEFT[4][0] = "O"
+
+SCENARIO_DANGER_RIGTH = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_RIGTH[0][0] = "J"
+SCENARIO_DANGER_RIGTH[4][7] = "O"
+
+SCENARIO_DANGER_UP = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_UP[0][0] = "J"
+SCENARIO_DANGER_UP[0][4] = "O"
+
+SCENARIO_DANGER_DOWN = [["" for j in range(COL)] for i in range(ROW)]
+SCENARIO_DANGER_DOWN[0][0] = "J"
+SCENARIO_DANGER_DOWN[7][4] = "O"
