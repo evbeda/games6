4a6d05666676fc6baf2ae38b554783c3e664596f
natilou <natali.fpaura@gmail.com>
2022-08-11 14:07:23 -0300

    refactoring constants

--- backgammon/tests/test_scenarios.py
+++ backgammon/tests/test_scenarios.py
@@ -1,4 +1,11 @@
-from backgammon.game.constants import MESSAGE_FP, MESSAGE_SP
+from backgammon.game.constants import (
+    MESSAGE_FP,
+    MESSAGE_SP,
+    WINNER_WHITE,
+    WINNER_BLACK,
+    TIE,
+    BLACK,
+    WHITE)
 
 initial_board = [
     [2, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 5],
@@ -30,28 +37,27 @@ board_3 = [
 next_turn_resume_true = {
     "board": "board",
     "dices": [1, 2],
-    "points": {"BLACK": 3, "WHITE": 2},
+    "points": {BLACK: 3, WHITE: 2},
     "number_of_turns": 20,
     "piece_captured": {"BLACK": 1, "WHITE": 0}
 }
 
 next_turn_resume_false_B = {
-    "result": "BLACK WINS",
-    "point": {"BLACK": 15, "WHITE": 2}
+    "result": WINNER_BLACK,
+    "point": {BLACK: 15, WHITE: 2}
 }
 
 next_turn_resume_false_W = {
-    "result": "WHITE WINS",
-    "point": {"BLACK": 10, "WHITE": 15}
+    "result": WINNER_WHITE,
+    "point": {BLACK: 10, WHITE: 15}
 }
 
 next_turn_resume_false_TIE = {
-    "result": "TIE",
-    "point": {"BLACK": 15, "WHITE": 15}
+    "result": TIE,
+    "point": {BLACK: 15, WHITE: 15}
 }
 
 next_turn_active = f"{next_turn_resume_true} \n {MESSAGE_FP} {MESSAGE_SP}"
-
 next_turn_message_B = f"{next_turn_resume_false_B} \n GAME OVER"
 next_turn_message_W = f"{next_turn_resume_false_W} \n GAME OVER"
 next_turn_message_TIE = f"{next_turn_resume_false_TIE} \n GAME OVER"
